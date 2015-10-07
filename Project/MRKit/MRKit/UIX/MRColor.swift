cc797f981749e4389d92f728103ba0f29054af0e
muthuraman <muthuraman@cricking.in>
2015-10-08 01:54:42 +0530

    Created project with Empty Base classes

---
+++ Project/MRKit/MRKit/UIX/MRColor.swift
@@ -0,0 +1,13 @@
+//
+//  MRColor.swift
+//  MRKit
+//
+//  Created by muthu on 08/10/15.
+//  Copyright (c) 2015 Muthu. All rights reserved.
+//
+
+import UIKit
+
+class MRColor: UIColor {
+   
+}
---
+++ Project/MRKit/MRKit/UIX/MRTextView.swift
@@ -0,0 +1,21 @@
+//
+//  MRTextView.swift
+//  MRKit
+//
+//  Created by muthu on 08/10/15.
+//  Copyright (c) 2015 Muthu. All rights reserved.
+//
+
+import UIKit
+
+class MRTextView: UITextView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
